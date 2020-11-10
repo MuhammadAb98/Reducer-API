664e98f59d7c73a38ced8e28fef7144c37674a87
MuhammadAb98 <56094500+MuhammadAb98@users.noreply.github.com>
2020-11-11 04:13:31 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{useReducer} from 'react';
+import Child2 from './Child2';
+import Parent from './Parent';
+
+
+function App() {
+  return (
+    <div>
+      <Parent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Parent.js
@@ -0,0 +1,15 @@
+import React,{useReducer} from 'react';
+import Child2 from './Child2.js';
+
+
+const Parent= (props) =>{
+   return (
+
+<div>
+      <Child2/>
+</div>
+      )}
+
+
+
+export default Parent;
